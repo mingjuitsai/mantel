8a0fd9d67bb59567e2e43960918299e8d875af9c
Ming Jui Tsai <mingjuitsai@Mings-MacBook-Pro.local>
2024-10-02 16:21:26 +0800

    Update eslint config and install jest

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,8 @@
 import globals from "globals";
-
+import js from "@eslint/js";
 
 export default [
+  js.configs.recommended,
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
   {languageOptions: { globals: globals.browser }},
 ];
